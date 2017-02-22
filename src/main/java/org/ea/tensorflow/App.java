7db0c9cf22bfb4fd8de0dc67dd5a2260e228fa63
Daniel Persson <mailto.woden@gmail.com>
2017-02-22 08:10:39 +0100

    First implementation of an app.

---
+++ src/main/java/org/ea/tensorflow/App.java
@@ -0,0 +1,15 @@
+package org.ea.tensorflow;
+
+import org.tensorflow.TensorFlow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! I'm using tensorflow version " + TensorFlow.version() );
+    }
+}
