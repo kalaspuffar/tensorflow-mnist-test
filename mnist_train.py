af20c8b70feb140594620dd686c1c2cacae154ad
Daniel Persson <mailto.woden@gmail.com>
2017-04-30 01:01:29 +0200

    Added name scopes.

--- mnist_train.py
+++ mnist_train.py
@@ -5,15 +5,15 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 import tensorflow as tf
 sess = tf.InteractiveSession()
 
-x = tf.placeholder(tf.float32, shape=[None, 784], name="input_tensor")
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
+x = tf.placeholder(tf.float32, shape=[None, 784], name='input_tensor')
+y_ = tf.placeholder(tf.float32, shape=[None, 10], name='labels')
 
 def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
+  initial = tf.truncated_normal(shape, stddev=0.1, name='W')
   return tf.Variable(initial)
 
 def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
+  initial = tf.constant(0.1, shape=shape, name='B')
   return tf.Variable(initial)
 
 def conv2d(x, W):
@@ -26,37 +26,48 @@ def max_pool_2x2(x):
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
-x_image = tf.reshape(x, [-1,28,28,1])
+x_image = tf.reshape(x, [-1,28,28,1], 'reshaped_image')
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+with tf.name_scope('conv_1'):
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+with tf.name_scope('conv_2'):
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64], name='flatten')
 
-keep_prob = tf.placeholder(tf.float32, name="keep_prob")
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+with tf.name_scope('fc_1'):
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+with tf.name_scope('dropout'):
+    keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
+with tf.name_scope('fc_2'):
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
-y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 y_conv = tf.identity(y_conv, name="output_tensor")
 
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+with tf.name_scope('xent'):
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+
+with tf.name_scope('train'):
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+with tf.name_scope('accuracy'):
+    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 builder = tf.saved_model.builder.SavedModelBuilder("./model")
 sess.run(tf.global_variables_initializer())
